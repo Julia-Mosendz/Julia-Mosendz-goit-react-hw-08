581a3ab7dce3c1bf5e4a5df16eddfdb31b0e79f6
Julia Mosendz <juliadibrova1@gmail.com>
2025-04-22 10:04:54 +0300

    fixing

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,28 @@ import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contacts/slice";
 import  filtersReducer  from "./filters/slice";
 import { authReducer } from "./auth/slice";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
 
-const store = configureStore({
-  reducer: { contacts: contactsReducer, filters: filtersReducer, auth: authReducer },
+const persistConfig = {
+  key: "contactsApp",
+  storage,
+  whitelist: ["token"],
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+
+export const store = configureStore({
+  reducer: { contacts: contactsReducer, filters: filtersReducer, auth: persistedAuthReducer },
+  middlewear: (getDefaultMiddlewear) => {
+    return getDefaultMiddlewear({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    });
+  }
 });
 
-export default store;
+export const persistor = persistStore(store);
